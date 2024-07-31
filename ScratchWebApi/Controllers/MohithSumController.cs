1a07df7eac1e24967041cc2d1b1f059c88d24df4
mohithna12 <mohithramn@outlook.com>
2024-07-31 12:09:39 -0700

    added sum controller

---
+++ ScratchWebApi/Controllers/MohithSumController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ScratchWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MohithSumController : ControllerBase
+    {
+        // GET: api/<MohithSumController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<MohithSumController>/5
+        [HttpGet("{a}/{b}")]
+        public int GetSum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
