46c8fe1374b253f3ea8cbbe81218bc6d1e600b47
Narendra Babu <netnaren@gmail.com>
2024-07-30 19:40:02 -0700

    Adding first version of Web API code

---
+++ ScratchWebApi/Controllers/GreetController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ScratchWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreetController : ControllerBase
+    {
+        // GET: api/<GreetController>
+        [HttpGet]
+        public string Get()
+        {
+            return "Hey There!!!";
+        }
+
+        // GET api/<GreetController>/5
+        [HttpGet("{userId}")]
+        public string Get(string userId)
+        {
+            return $"Hello {userId} how are you doing?";
+        }
+
+    }
+}
